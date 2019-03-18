a22f11aa27783922fe841cd824dbd2eab901b184
chinmaya <chinmayasa@gmail.com>
2019-03-19 00:56:12 +0530

    employee server side

---
+++ employee-server/src/test/java/com/sahu/chinmaya/employeeserver/EmployeeServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sahu.chinmaya.employeeserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
